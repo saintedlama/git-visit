89e16f33a345db89066f12afb09673f7ad57c38a
Christoph Walcher <christoph.walcher@softwarepark.cc>
2016-12-05 21:01:34 +0100

    Remove useless tests and use local git repo for tests

--- test/index.js
+++ test/index.js
@@ -5,26 +5,29 @@ var rimraf = require('rimraf');
 var expect = require('chai').expect;
 var Repository = require('../');
 
+const remoteUrl = path.join(__dirname, 'fixtures', 'repo');
+const cloneDir = path.join(__dirname, '..', '..', 'git-visit-test-tmp');
+
 describe('repository', function() {
-  var testDir = 'test_tmp/visit/mongoose-version';
+  this.timeout(5000);
+
+
   beforeEach(function() {
-    rimraf.sync(testDir);
+    rimraf.sync(cloneDir);
   });
 
   describe('show', function() {
-    it('should show file contents of a specific revision [endtoendtest]', function(next) {
-      this.timeout(50000);
+    it('should show file contents of a specific revision', function(next) {
 
-      var expectedContents = fs.readFileSync(path.join(__dirname,'fixtures', 'README.md'), { encoding : 'utf-8' });
-
-      var repo = new Repository(testDir, 'https://github.com/saintedlama/mongoose-version.git');
+      var repo = new Repository(cloneDir, remoteUrl);
       repo.update(function(err) {
         expect(err).to.not.exist;
 
-        repo.show('README.md', 'cd7833b1cefdec4c12fe648cb6ccbc7508fbf222', function(err, contents) {
+        repo.show('README.md', 'c43f0f08c9da96b951aabce724795a20ed8149ce', function(err, contents) {
           expect(err).to.not.exist;
 
-          expect(contents).to.equal(expectedContents);
+          expect(contents).to.contain('Change 1');
+          expect(contents).to.not.contain('Change 2');
 
           next();
         })
@@ -33,9 +36,7 @@ describe('repository', function() {
   });
 
   describe('visit', function() {
-    it('should clone the repo if the repo does not exist [endtoendtest]', function(next) {
-      this.timeout(50000);
-
+    it('should clone the repo if the repo does not exist', function(next) {
       var visitor = {
         counter : 0,
         visit : function(repo, commit, cb) {
@@ -44,87 +45,17 @@ describe('repository', function() {
         }
       };
 
-      var repo = new Repository(testDir, 'https://github.com/saintedlama/mongoose-version.git');
+      var repo = new Repository(cloneDir, remoteUrl);
       repo.visit(visitor, function(err) {
         expect(err).to.not.exist;
-        expect(visitor.counter).to.equal(70);
+        expect(visitor.counter).to.equal(5);
 
         next();
       });
     });
 
-    it('should clone the repo with a private key if given in options [endtoendtest]', function(next) {
-      this.timeout(50000);
-
-      var visitor = {
-        counter : 0,
-        visit : function(repo, commit, cb) {
-          this.counter ++;
-          cb();
-        }
-      };
-
-      var repo = new Repository(testDir, 'https://github.com/saintedlama/mongoose-version.git', { privateKey : 'a0' });
-      repo.visit(visitor, function(err) {
-        expect(err).to.not.exist;
-        expect(visitor.counter).to.equal(70);
-
-        next();
-      });
-    });
-
-    it('should pass results of visitor.visit to callback [endtoendtest]', function(next) {
-      this.timeout(50000);
-
-      var visitor = {
-        visit : function(repo, commit, cb) {
-          cb(null, 1);
-        }
-      };
-
-      var repo = new Repository(testDir, 'https://github.com/saintedlama/mongoose-version.git');
-      repo.visit(visitor, function(err, results) {
-        expect(err).to.not.exist;
-
-        expect(results).to.exist;
-        expect(results.length).to.equal(70);
-
-        results.forEach(function(result) {
-          expect(result).to.equal(1);
-        });
-
-        next();
-      });
-    });
-
-    it('should use git ssh wrapper if a private key is specified [endtoendtest]', function(next) {
-      this.timeout(50000);
-
-      var visitor = {
-        visit : function(repo, commit, cb) {
-          cb(null, 1);
-        }
-      };
-
-      var repo = new Repository(testDir, 'https://github.com/saintedlama/mongoose-version.git', { privateKey : 'key' });
-      repo.visit(visitor, function(err, results) {
-        expect(err).to.not.exist;
-
-        expect(results).to.exist;
-        expect(results.length).to.equal(70);
-
-        results.forEach(function(result) {
-          expect(result).to.equal(1);
-        });
-
-        next();
-      });
-    });
-
-    it('should get a parsed git diff numstat [endtoendtest]', function(next) {
-      this.timeout(50000);
-
-      var repo = new Repository(testDir, 'https://github.com/saintedlama/mongoose-version.git', { privateKey : 'key' });
+    it('should get a parsed git diff numstat', function(next) {
+      var repo = new Repository(cloneDir, remoteUrl);
 
       repo.update(function(err) {
         expect(err).to.not.exist;
@@ -136,8 +67,8 @@ describe('repository', function() {
             expect(err).to.not.exist;
 
             expect(diffs).to.exist;
-            expect(diffs[0].path).to.equal('.gitignore');
-            expect(diffs[0].added).to.equal(16);
+            expect(diffs[0].path).to.equal('README.md');
+            expect(diffs[0].added).to.equal(7);
             expect(diffs[0].deleted).to.equal(0);
 
             next();
